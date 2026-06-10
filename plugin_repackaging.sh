349c5797e2e8e7455960ecbb79fb41be8b0ca226
YAO <yyf1986@users.noreply.github.com>
2026-06-10 17:47:27 +0800

    Update plugin_repackaging.sh

--- plugin_repackaging.sh
+++ plugin_repackaging.sh
@@ -333,9 +333,9 @@ PY
 
 	mkdir -p ./wheels
 	echo "Downloading wheels to ./wheels/..."
-	${PIP_CMD} download ${PIP_PLATFORM} --prefer-binary -r requirements.txt -d ./wheels \
-	#${PIP_CMD} download --prefer-binary -r requirements.txt -d ./wheels \
-		--index-url ${PIP_MIRROR_URL} --trusted-host mirrors.aliyun.com
+	#${PIP_CMD} download ${PIP_PLATFORM} --prefer-binary -r requirements.txt -d ./wheels \
+	${PIP_CMD} download --prefer-binary -r requirements.txt -d ./wheels \
+		--index-url ${PIP_MIRROR_URL} --trusted-host mirrors.aliyun.com --only-binary=:all:
 	if [[ $? -ne 0 ]]; then
 		echo "✗ Error: Failed to download dependencies"
 		exit 1
